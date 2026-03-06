3eb7110a9a605d6fb6fd14facc074f54153eeeb2
Fishmaia <fishmaia2@gmail.com>
2026-03-06 20:19:10 -0300

    made a number guesser game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Quic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool incorrect = true;
+        string guess = "-3";
+        int intGuess = -2;
+        int guessCounter = 1;
+        string answer = "default";
+        bool quit = false;
+
+        while (quit == false)
+        {
+
+            Random randomGenerator = new Random();
+            int MagicNumber = randomGenerator.Next(1,100);
+
+            while (incorrect == true)
+            {
+                Console.Write("What is the magic number? ");
+                guess = Console.ReadLine();
+                intGuess = int.Parse(guess);
+                if (MagicNumber>intGuess)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else if (MagicNumber<intGuess)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCounter} tries!");
+                    incorrect = false;
+                }
+                guessCounter += 1;
+            }
+            Console.Write("Would you like to play again? ");
+            answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    incorrect=true;
+                }
+                else
+                {
+                    quit=true;
+                }
+        
+
+        }
     }
 }
