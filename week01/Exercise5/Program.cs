80e3dc15e313a6193bc27373edab3ee9aad52de4
MayaTheMonster <mayaisthemonster@gmail.com>
2026-03-07 16:53:40 -0300

    Made several static functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void SayHello()
+        {
+            Console.WriteLine("Welcome to the Program");
+        }
+        
+        static string AskName()
+        {
+            Console.Write("Please enter your name: ");
+            string nameUser = Console.ReadLine();
+            return nameUser;
+        }
+
+        static int AskWholeNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string Numb1 = Console.ReadLine();
+            int numb2 = int.Parse(Numb1);
+            return numb2;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int square = number*number;
+            return square;
+        }
+        
+        static void DisplaySquareName(string name, int square)
+        {
+            Console.WriteLine($"Brother {name}, the square of your number is {square}");
+        }
+
+        static void main()
+        {
+            SayHello();
+            string username = AskName();
+            int favoriteNumber = AskWholeNumber();
+            int squaredFavorite = SquareNumber(favoriteNumber);
+            DisplaySquareName(username,squaredFavorite);
+        }
+        main();
     }
 }
